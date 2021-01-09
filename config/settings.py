6c1fb5934f6728560b2bb7ccac5a490fbf31d6bb
minsgy <alstjrchl97@naver.com>
2021-01-09 11:14:21 +0900

    [minseok] : Setting AUTH MODEL 설정, USER 모델 및 VIEW, ADMIN 페이지 구현

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
