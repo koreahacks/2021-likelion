7eb2de19212bedb5fc8440c0cbe92951ec84b5c7
ningpop <craft1933@naver.com>
2021-01-09 16:18:27 +0900

    [namjoon] : add base.html with static files

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,11 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # 개발자 관리 파일들을 static에 저장.
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # 개발자 관리 파일들을 static에 저장.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # media 파일 url 설정
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
