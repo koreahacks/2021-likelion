6d04754332f6cf14a46b63f5a438f4a088955b96
KimTaewan <wwan13@naver.com>
2021-01-09 16:03:58 +0900

    [taewan] : add contest delete logic

--- contests/urls.py
+++ contests/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("create/", views.create_contest, name="contest_create"),
     path("list/", views.display_contest_list, name="contest_list"),
+    path("<int:contest_id>/delete/", views.delete_contest, name="contest_delete"),
 ]
