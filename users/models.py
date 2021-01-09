a43dea1355d26b5fef7594ac37f12dededbd8de0
KimTaewan <wwan13@naver.com>
2021-01-10 04:48:36 +0900

    [taewan] : register

--- users/models.py
+++ users/models.py
@@ -6,27 +6,30 @@ from django_fields import DefaultStaticImageField
 
 
 class HashTags(models.Model):
-    ''' 자기 소개용 해시태그 Model '''
-    tag_name = models.CharField(max_length=20) # 해시태그이름
-    user = models.ForeignKey( # User 외래키
-        "users.User", related_name="hashtags", on_delete=models.CASCADE)
-    
+    """ 자기 소개용 해시태그 Model """
+
+    tag_name = models.CharField(max_length=20)  # 해시태그이름
+    user = models.ForeignKey(  # User 외래키
+        "users.User", related_name="hashtags", on_delete=models.CASCADE
+    )
+
     def __str__(self):
         return self.tag_name
 
+
 class User(AbstractUser):
-    
-    ''' 연결된 모델 '''
+
+    """ 연결된 모델 """
+
     # HashTags 1:N
     # Contest N:M
 
     # 구현해야 할 목록
     # 뱃지 이벤트들 구현해야함 ..
-    
 
-    ''' 사용자 + 포트폴리오 모델  '''
+    """ 사용자 + 포트폴리오 모델  """
     name = models.CharField(max_length=20)  # 이름
-    image = DefaultStaticImageField(upload_to='images/', blank=True)  # 프로필 사진
+    image = DefaultStaticImageField(upload_to="images/", blank=True)  # 프로필 사진
     phone_number = models.CharField(max_length=20)  # 전화번호
     email = models.CharField(max_length=30)  # 이메일
 
@@ -44,4 +47,3 @@ class User(AbstractUser):
         self.hit_count = self.hit_count + 1
         self.save()
         return self.hit_count
-
